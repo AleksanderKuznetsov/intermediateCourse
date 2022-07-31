84bca30652a5575c12d6d3e844bb1327ccf3d97c
AleksanderKuznetsov <0504728348a@gmail.com>
2022-07-31 13:34:45 +0300

    add Recursion_1.py

---
+++ Recursion_1.py
@@ -0,0 +1,13 @@
+"""
+Raising the number N to the power M.
+"""
+
+
+def degree(N, M):
+    """
+    :param N: number
+    :param M: degree
+    """
+    if M == 0:
+        return 1
+    return degree(N, M-1) * N
